320b06b4ed375d8d162d5a6860139012998ff5ed
Parasmani Chand <138095406+parasmani-chand@users.noreply.github.com>
2025-06-27 11:23:28 +0000

    variable and constant done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 456454
+let accountEmail = "janimala@gmail.com"
+var accountPassword = "23552"
+accountCity = "Kathmandu"
+let accountState;
+
+// accountId = 2   // not allowed
+accountEmail = "paras@gmail.com"
+accountPassword = "2355646"
+accountCity = "Lalitpur"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
